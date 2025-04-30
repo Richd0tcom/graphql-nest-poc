24d4ec9d2fc40f8e91dbac966319072d48bc1b1e
Inyeneobong Ekwere <tuberich0@gmail.com>
2025-04-30 13:24:21 +0100

    fx: validation fixes

--- src/auth/dto/create-auth.input.ts
+++ src/auth/dto/create-auth.input.ts
@@ -1,25 +1,23 @@
 import { InputType, Field } from "@nestjs/graphql";
-import { IsEmail, MinLength } from "class-validator";
+import { MinLength } from "class-validator";
 
 @InputType()
 export class CreateAuthInput {
   @Field()
-  @IsEmail()
   username: string;
 
   @Field()
-  @MinLength(7)
+  @MinLength(2)
   password: string;
 }
 
 @InputType()
 export class LoginInput {
   @Field()
-  @IsEmail()
   username: string;
 
   @Field()
-  @MinLength(7)
+  @MinLength(2)
   password: string;
 }
 
